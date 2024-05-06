40c7aafc144fc9db906d695dc235980a75b6db6d
Ronald <ronald.jian@gmail.com>
2024-05-07 04:14:18 +0800

    install vite-plugin-compression

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
     plugins: [
@@ -8,7 +9,13 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/Livewire/**',
+            ],
+            detectTls: 'takephoto.ink.test',
         }),
+
+        viteCompression(),
     ],
 });
