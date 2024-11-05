d0279c89524893323b4e99fc6da1dad85d929f76
Ronald <ronald.jian@gmail.com>
2024-11-06 05:24:24 +0800

    change livewire layout name to main.blade.php and move to layouts folder

---
+++ config/livewire.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |---------------------------------------------------------------------------
+    | Layout
+    |---------------------------------------------------------------------------
+    | The view that will be used as the layout when rendering a single component
+    | as an entire page via `Route::get('/post/create', CreatePost::class);`.
+    | In this case, the view returned by CreatePost will render into $slot.
+    |
+    */
+
+    'layout' => 'layouts.main',
+];
