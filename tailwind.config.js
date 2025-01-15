5e74dcefd5d4952d275c73c622f6d86ed928ab3f
Ronald <ronald.jian@gmail.com>
2025-01-15 11:54:28 +0800

    close tailwindcss dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: false, // 禁用深色模式
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
